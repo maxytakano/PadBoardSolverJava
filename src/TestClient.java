eacd4c72d6fe745d02b9c19aa4c3893d56455d81
mtakano <max.takano@sony.com>
2018-06-01 13:29:35 -0700

    add board validity check

--- src/TestClient.java
+++ src/TestClient.java
@@ -1,16 +1,22 @@
 import edu.princeton.cs.algs4.*;
+import java.util.Arrays;
 
 public class TestClient {
   public static void main(String[] args) {
     // Create initial board from file
     In in = new In(args[0]);
     int size = in.readInt();
+    int[] startCounts = new int[20];
+    int[] endCounts = new int[20];
+    int nextCell;
 
     // Read starting board position
     int[][] startPosition = new int[size][size];
     for (int row = 0; row < size; row++) {
       for (int col = 0; col < size; col++) {
-        startPosition[row][col] = in.readInt();
+        nextCell = in.readInt();
+        startCounts[nextCell]++;
+        startPosition[row][col] = nextCell;
       }
     }
 
@@ -18,9 +24,17 @@ public class TestClient {
     int[][] endPosition = new int[size][size];
     for (int row = 0; row < size; row++) {
       for (int col = 0; col < size; col++) {
-        endPosition[row][col] = in.readInt();
+        nextCell = in.readInt();
+        endCounts[nextCell]++;
+        endPosition[row][col] = nextCell;
       }
     }
+
+    boolean boardValid = Arrays.equals(startCounts, endCounts);
+    System.out.println("Board valid: " + boardValid);
+
+    long startTime = System.currentTimeMillis();
+    System.out.println("start solve");
 
     // Create board with start and end positions
     Board board = new Board(startPosition, endPosition);
@@ -30,6 +44,9 @@ public class TestClient {
 
     if (solver.isSolvable()) {
       StdOut.println("Minimum number of moves = " + solver.minMoves());
+      long endTime = System.currentTimeMillis();
+      long duration = (endTime - startTime);
+      System.out.println("Time taken: " + duration);
       for (Board b : solver.solution()) {
         StdOut.println(b);
       }
